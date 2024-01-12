8b90a81aab31876123845e2ea132583023fd8dd3
Samsonium <tdesu@vk.com>
2024-01-12 08:54:40 +0300

    Initial commit

---
+++ src/main/java/ru/samsonium/primate/world/PrimateWorld.java
@@ -0,0 +1,17 @@
+package ru.samsonium.primate.world;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrimateWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
